a812cbfeb78efa5381775b0eb69fbdb11ac3e39f
Amit Saini <amitssaini999@gmail.com>
2024-11-27 12:25:08 +0530

    changes bothend

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,20 @@ const cors = require('cors')
 const app = express();
 const userRouter = require('./Routes/user.route')
 
+//DB_URI =mongodb+srv://admin:pa66word@cluster0.7ugfj.mongodb.net/user-db?retryWrites=true&w=majority&appName=Cluster0
 
-const port = process.env.PORT;
+
+const PORT = process.env.PORT;
 const db_uri = process.env.DB_URI
-
 mongoose
 .connect(db_uri)
 .then(()=>{
     console.log(`DB IS CONNECTED`)
-    app.listen(port,()=>console.log(`Server is Connected at the port ${port}`))
+    
 })
 .catch((error)=>console.log('DB IS NOT CONNECTED'))
+
+app.listen(PORT,()=>console.log(`Server is Connected at the port ${PORT}`))
 
 const corsOptions = {
     origin: 'http://localhost:3000', // Replace with your frontend's URL
